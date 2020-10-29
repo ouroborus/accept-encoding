b7f11f828401dff903e3d302ce87ebc6a0d5702b
ouroborus <deadchicken@gmail.com>
2020-10-28 21:12:33 -0700

    update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import json
+
 def app(environ, start_response):
   #data = b"Hello, World!\n"
-  data = '\n'.join([
-    '%s: %s' % (key, value) for key, value in sorted(environ.items())
-  ])
+  #data = '\n'.join([
+  #  '%s: %s' % (key, value) for key, value in sorted(environ.items())
+  #]).encode('utf-8')
+  #data = json.dumps({ 'accept-encoding': environ.get('HTTP_ACCEPT_ENCODING', None) }).encode('utf-8')
+  data = json.dumps({str(k):str(v) for k, v in environ.items()}).encode('utf-8')
   start_response("200 OK", [
-    ("Content-Type", "text/plain"),
+    ("Content-Type", "application/json"),
     ("Content-Length", str(len(data)))
   ])
-  return iter([data])
+  return [data]
