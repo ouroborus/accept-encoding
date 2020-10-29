7029b38a9663277fc5520072c0887fab80529f76
ouroborus <deadchicken@gmail.com>
2020-10-28 20:41:01 -0700

    fix app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def app(environ, start_response):
   #data = b"Hello, World!\n"
-  response_body = '\n'.join([
+  data = '\n'.join([
     '%s: %s' % (key, value) for key, value in sorted(environ.items())
   ])
   start_response("200 OK", [
