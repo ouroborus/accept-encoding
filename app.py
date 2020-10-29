447999ad7f3591dfb260790ff55ce4b227a30580
ouroborus <deadchicken@gmail.com>
2020-10-29 14:53:26 -0700

    update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import json
 
+origins = set([
+  'https://nearest.ouroborus.org',
+])
+
 def app(environ, start_response):
-  #data = b"Hello, World!\n"
-  #data = '\n'.join([
-  #  '%s: %s' % (key, value) for key, value in sorted(environ.items())
-  #]).encode('utf-8')
-  #data = json.dumps({ 'accept-encoding': environ.get('HTTP_ACCEPT_ENCODING', None) }).encode('utf-8')
+  origin = environ.get('HTTP_ORIGIN', None)
+  if origin is None or origin not in origins:
+    start_response("403 Forbidden", [
+      ("Vary", "Origin"),
+    ])
+    return []
+  
+  accept_encoding = environ.get('HTTP_ACCEPT_ENCODING', None)
+  if accept_encoding is None:
+    accept_encoding = 'null'
+  else:
+    accept_encoding = '"' + accept_encoding + '"'
+  
+  #data = ('{"Accept-Encoding":' + accept_encoding + '}').encode('utf-8')
   data = json.dumps({str(k):str(v) for k, v in environ.items()}).encode('utf-8')
+  
   start_response("200 OK", [
     ("Content-Type", "application/json"),
-    ("Content-Length", str(len(data)))
+    ("Access-Control-Allow-Origin", origin),
   ])
   return [data]
